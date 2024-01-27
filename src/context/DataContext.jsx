859766d231482b1c40cf4b091c7aa0b23f79ce8d
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-27 19:18:20 +0100

    refactor database

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -8,23 +8,23 @@ export const DataContext = createContext(null);
 const arrRutinas = [];
 const arrNutricion = [];
 const getData= async ()=>{
-
-  const collectionRutinas = await getDocs(collection(db, "Rutinas"));
-  collectionRutinas.forEach((doc) => {
-    arrRutinas.push(doc.data());
-  });
-  const collectionNutricion = await getDocs(collection(db, "guiaNutricion"));
-  collectionNutricion.forEach((doc) => {
-    arrNutricion.push(doc.data());
-  });
-}
-getData()
+    
+      const collectionRutinas = await getDocs(collection(db, "Rutinas"));
+      collectionRutinas.forEach((doc) => {
+        arrRutinas.push(doc.data());
+      });
+      const collectionNutricion = await getDocs(collection(db, "guiaNutricion"));
+      collectionNutricion.forEach((doc) => {
+        arrNutricion.push(doc.data());
+      });
+    }
 
 export const DataContextProvider = ({ children }) => {
   const [rutinas, setRutinas] = useState();
   const [nutricion, setNutricion] = useState();
-
+  
   useEffect(() => {
+    
     getData().then(data=>{
       console.log(data);
       setNutricion(arrNutricion)
